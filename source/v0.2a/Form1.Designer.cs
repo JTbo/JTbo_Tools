321d42e7dd4f30df68252939e8e2fb9a1468e4e6
jtbo <jtbofin@gmail.com>
2012-11-17 15:42:46 +0200

    Implemented save function, test with caution!

---
+++ source/v0.2a/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TGM_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonClcTireS = new System.Windows.Forms.Button();
+            this.textBox_size3 = new System.Windows.Forms.TextBox();
+            this.textBox_size2 = new System.Windows.Forms.TextBox();
+            this.textBox_size1 = new System.Windows.Forms.TextBox();
+            this.textBox_fn = new System.Windows.Forms.TextBox();
+            this.button_generate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(621, 542);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(621, 571);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 626);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(684, 233);
+            this.textBox1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonClcTireS);
+            this.groupBox1.Controls.Add(this.textBox_size3);
+            this.groupBox1.Controls.Add(this.textBox_size2);
+            this.groupBox1.Controls.Add(this.textBox_size1);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(547, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(149, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New tire size";
+            this.groupBox1.Visible = false;
+            // 
+            // buttonClcTireS
+            // 
+            this.buttonClcTireS.Location = new System.Drawing.Point(18, 60);
+            this.buttonClcTireS.Name = "buttonClcTireS";
+            this.buttonClcTireS.Size = new System.Drawing.Size(75, 23);
+            this.buttonClcTireS.TabIndex = 1;
+            this.buttonClcTireS.Text = "Calculate";
+            this.buttonClcTireS.UseVisualStyleBackColor = true;
+            // 
+            // textBox_size3
+            // 
+            this.textBox_size3.Location = new System.Drawing.Point(99, 19);
+            this.textBox_size3.Name = "textBox_size3";
+            this.textBox_size3.Size = new System.Drawing.Size(30, 20);
+            this.textBox_size3.TabIndex = 0;
+            // 
+            // textBox_size2
+            // 
+            this.textBox_size2.Location = new System.Drawing.Point(63, 19);
+            this.textBox_size2.Name = "textBox_size2";
+            this.textBox_size2.Size = new System.Drawing.Size(30, 20);
+            this.textBox_size2.TabIndex = 0;
+            // 
+            // textBox_size1
+            // 
+            this.textBox_size1.Location = new System.Drawing.Point(7, 20);
+            this.textBox_size1.Name = "textBox_size1";
+            this.textBox_size1.Size = new System.Drawing.Size(50, 20);
+            this.textBox_size1.TabIndex = 0;
+            // 
+            // textBox_fn
+            // 
+            this.textBox_fn.Location = new System.Drawing.Point(12, 600);
+            this.textBox_fn.Name = "textBox_fn";
+            this.textBox_fn.ReadOnly = true;
+            this.textBox_fn.Size = new System.Drawing.Size(684, 20);
+            this.textBox_fn.TabIndex = 6;
+            this.textBox_fn.TabStop = false;
+            // 
+            // button_generate
+            // 
+            this.button_generate.Enabled = false;
+            this.button_generate.Location = new System.Drawing.Point(346, 553);
+            this.button_generate.Name = "button_generate";
+            this.button_generate.Size = new System.Drawing.Size(75, 23);
+            this.button_generate.TabIndex = 7;
+            this.button_generate.Text = "Generate";
+            this.button_generate.UseVisualStyleBackColor = true;
+            this.button_generate.Click += new System.EventHandler(this.button_Generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 871);
+            this.Controls.Add(this.button_generate);
+            this.Controls.Add(this.textBox_fn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "TGM Tool v0.2a ALPHA Version, use at your own risk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonClcTireS;
+        private System.Windows.Forms.TextBox textBox_size3;
+        private System.Windows.Forms.TextBox textBox_size2;
+        private System.Windows.Forms.TextBox textBox_size1;
+        private System.Windows.Forms.TextBox textBox_fn;
+        private System.Windows.Forms.Button button_generate;
+    }
+}
+
---
+++ source/v0.2a/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TGM_reader
+{
+    public partial class Form1 : Form
+    {
+        int size = -1;
+        string[] nodes = new string[200];
+        int linenum = 0;
+        string line;
+        string v;
+        string file;
+        string filename;
+        string text;
+        Regex g = new Regex(@"Geometry=\(*");
+        int u = 0;
+        int i = 0;
+        string[] results2 = new string[33];
+        string[] results3 = new string[33];
+        string[] results4 = new string[33];
+        string[] resultso2 = new string[33];
+        string[] resultso3 = new string[33];
+        string[] resultso4 = new string[33];
+        TextBox[] txtNodeNames = new TextBox[33];
+        TextBox[] txtNodeNames2 = new TextBox[33];
+        TextBox[] txtNodeNames3 = new TextBox[33];
+        Regex g2 = new Regex(@"\-?\d{1}\.?[0-9]*");
+        Regex g3 = new Regex(@"\,\-\d{1}\.[0-9]*\,");
+        Regex g4 = new Regex(@"\,\d{1}\.[0-9]*\)");
+        double math;
+        int n = -2;
+        string[] writeresult = new string[33];
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+        
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+      
+            
+
+            
+        
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //class Program
+
+            //{
+            //static void Main()
+            //{
+            textBox1.Text = "";
+           // if (i > 0)
+            //{
+                int r1=0;
+                while (r1 < linenum)
+                {
+                    this.Controls.Remove(txtNodeNames[r1]);
+                    this.Controls.Remove(txtNodeNames2[r1]);
+                    this.Controls.Remove(txtNodeNames3[r1]);
+                    r1++;
+                }
+            //}
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+                filename = openFileDialog1.FileName;
+                try
+                {
+                    text = File.ReadAllText(file);
+                    size = text.Length;
+                    //textBox1.Text = text;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            
+            
+             // (@"Geometry=\(($)*");
+            textBox_fn.Text = filename;
+            using (StreamReader r = new StreamReader(filename))
+            {
+                linenum = 0;
+                while ((line = r.ReadLine()) != null)
+                {
+
+                    // X.
+                    // Try to match each line against the Regex.
+                    Match m = g.Match(line);
+                    if (m.Success)
+                    {
+
+                        // Y.
+                        // Write original ine and the value.
+
+                        v = m.Groups[linenum].Value;
+                        //textBox1.AppendText(line);
+                        //textBox1.AppendText("\t"+v);
+                        
+                        if (line != null)
+                        {
+                            nodes[linenum] = line;
+                        }
+                        
+                        linenum++;
+                    }
+                }
+                u = 0;
+                i = 0;
+
+
+                    while (i <= (linenum/2)) {// nodes.Length; )
+                    //try
+                    //{
+                        
+                        Match m2 = g2.Match(nodes[u]);
+                        
+                        Match m3 = g3.Match(nodes[u]);
+                        
+                        Match m4 = g4.Match(nodes[u]);
+                        //textBoxNode0_2.Text = m2.Groups[0].Value;
+                        results2[u] = m2.Groups[0].Value;
+                        //textBoxNo0.Text = results2[u];
+                        results3[u] = m3.Groups[0].Value;
+                        results4[u] = m4.Groups[0].Value;
+                        results3[u] = Regex.Replace(results3[u], "\\,", "");
+                        results4[u] = Regex.Replace(results4[u], "\\)", "");
+                        results4[u] = Regex.Replace(results4[u], "\\,", "");
+                        txtNodeNames[u] = new TextBox();
+                        txtNodeNames2[u] = new TextBox();
+                        txtNodeNames3[u] = new TextBox();
+                            //string m2name = m2.Groups[i].Value;
+                            //var txt = new TextBox();
+                            //txtNodeNames[i] = txt;
+                            this.Controls.Add(txtNodeNames[u]);
+                            this.Controls.Add(txtNodeNames2[u]);
+                            this.Controls.Add(txtNodeNames3[u]);
+                            //txtNodeNames[u].Name = "txtbox"+u;
+                            txtNodeNames[u].Text = results2[u];
+                            txtNodeNames2[u].Text = results3[u];
+                            txtNodeNames3[u].Text = results4[u];
+                            if (u < 17)
+                            {
+                                txtNodeNames[u].Location = new Point(227, 27 + (u * 28));
+                                txtNodeNames2[u].Location = new Point(337, 27 + (u * 28));
+                                txtNodeNames3[u].Location = new Point(447, 27 + (u * 28));
+                            }
+                            else {
+                                txtNodeNames[u].Location = new Point(347, 27 + ((u-16) * 28));
+                                txtNodeNames2[u].Location = new Point(437, 27 + ((u - 16) * 28));
+                                txtNodeNames3[u].Location = new Point(547, 27 + ((u - 16) * 28));
+                            }
+                            txtNodeNames[u].Visible = true;
+                            txtNodeNames2[u].Visible = true;
+                            textBox1.AppendText(nodes[u] + Environment.NewLine);
+                            //textBoxNode0_1.Text = nodes[u];
+                            u++;                      
+                            i++;
+                    }
+
+                    //n = -1;
+                    n = u;
+                    u=u-2;
+                    textBox1.AppendText(" " + Environment.NewLine);
+                while (u >= 0){
+                    math=0;
+                    math = Double.Parse(results2[u], System.Globalization.CultureInfo.InvariantCulture);
+                    math = math * -1;
+                    results2[n] = Convert.ToString(math);
+                    textBox1.AppendText("Geometry=("+results2[n] +","+results3[u]+","+results4[u]+")"+Environment.NewLine);
+                    
+                    u--;
+                    n++;
+                    //n=n-2;
+                }
+
+                        /*  if (i == 1)
+                          {
+                              textBoxNode0.Text = v2;
+                              textBoxNode0_1.Text = Regex.Replace(v3, "\\,", "");
+                              textBoxNode0_2.Text = Regex.Replace(v4, "\\)", "");
+                          }
+                          if (i == 2)
+                          {
+                              textBoxNode1.Text = v2;
+                              textBoxNode1_1.Text = Regex.Replace(v3, "\\,", "");
+                              textBoxNode1_2.Text = Regex.Replace(v4, "\\)", "");
+                          }
+                      }
+                      textBox1.AppendText(nodes[i]);*/
+                  /*  catch
+                    {
+                        if (line == null)
+                        {
+                            break;
+                        }
+                    }
+                    finally
+                    {
+                        if (line == null)
+                        {
+
+                        }
+                    }
+                    } */
+                    
+            }
+            button_generate.Enabled = true;
+       //  abs runProgram = new Program();
+            }
+        //}  
+        //}
+
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            DialogResult result = saveFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+                filename = saveFileDialog1.FileName;
+                //string line = null;
+                int linew=0;
+                string[] tgm = new string[5000];
+                int rline=0;
+                u=0;
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    while ((tgm[rline]=reader.ReadLine()) != null)
+                            {
+                              //  tgm[rline] = reader.;
+                        Match mw = g.Match(tgm[rline]);
+                        if (mw.Success)
+                        {
+
+                            tgm[rline] = writeresult[u];
+                            u++;
+
+                        }
+                        //sline++;
+                                rline++;
+                            }
+                   // int sline=0;
+                    while (linew <= rline)
+                    {
+                        
+                        
+                       
+                            using (StreamWriter w = new StreamWriter(filename, true))
+                            {
+                                w.WriteLine(tgm[linew]);
+                                textBox1.AppendText(tgm[linew]+"\r\n");
+                            }
+                        linew++;
+                            
+                        }
+                    
+                        
+                    
+                }
+            }
+            
+        }
+
+
+
+            
+            
+            
+            /*  int size = -1;
+              DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+              if (result == DialogResult.OK) // Test result.
+              {
+                  string file = openFileDialog1.FileName;
+                  filename = openFileDialog1.FileName;
+                  try
+                  {
+                      string text = File.ReadAllText(file);
+                      size = text.Length;
+                      textBox1.Text = text;
+                  }
+                  catch (IOException)
+                  {
+                  }
+              }*/
+        
+
+        private void button_Generate_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            u = 0;
+            n = 0;
+            while (u <= linenum /2)
+            {
+                resultso2[u] = txtNodeNames[u].Text;
+                resultso3[u] = txtNodeNames2[u].Text;
+                resultso4[u] = txtNodeNames3[u].Text;
+
+                u++;
+            }
+            u = u-1;
+            n = (linenum / 2);
+                    while (u >= 0){
+                    math=0;
+                    math = Double.Parse(resultso2[u], System.Globalization.CultureInfo.InvariantCulture);
+                    math = math * -1;
+                    resultso2[n] = Convert.ToString(math);
+                    resultso3[n] = resultso3[u];
+                    resultso4[n] = resultso4[u];
+                    n++;
+                    u--;
+            }
+            n = 0;
+            while (n < linenum)
+            {
+                textBox1.AppendText("Geometry=(" + resultso2[n] + "," + resultso3[n] + "," + resultso4[n] + ")" + Environment.NewLine);
+                writeresult[n] = ("Geometry=(" + resultso2[n] + "," + resultso3[n] + "," + resultso4[n] + ")");
+                n++;
+            }
+            btnSave.Enabled = true;
+        }
+    }
+}
